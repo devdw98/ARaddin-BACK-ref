87b4debbc97f55b5122f1f873e18f6f79ffa1bb2
dw <devdw98@gmail.com>
2021-07-05 00:08:29 +0900

    setting api router

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { firebaseFile } from './vars';
 
 import { initialFirebase } from './utils/firebase';
+import router from './api/_index';
 
 // Create Express server
 const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
+
+app.use('/api', router);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
