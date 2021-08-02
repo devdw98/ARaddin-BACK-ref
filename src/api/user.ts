ab991ed056576d42454f513b1498e4412ee98075
dw <devdw98@gmail.com>
2021-08-02 16:24:23 +0900

    upload photofiles

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,7 +5,18 @@ import { IUser } from '../models/user';
 import { checkFirebase } from '../utils/firebase';
 import * as UserDao from '../dao/user';
 import { photoPath } from '../vars';
-import { UploadedFile } from 'express-fileupload';
+import multer from 'multer';
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, photoPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 
 const loginScheme = yup.object({
   token: yup.string().required(),
@@ -13,62 +24,65 @@ const loginScheme = yup.object({
   photos: yup.array(),
 });
 
-//upload file
-function uploadPhoto(userPhotoPath: string, files: UploadedFile[]) {
-  const path = photoPath + userPhotoPath;
-  let i = 1;
-  for (const f of files) {
-    if (f.truncated) {
-      return false;
-    }
-    f.mv(path + `/photo${i}.jpg`);
-    i++;
-  }
-  return true;
-}
-
 async function login(req: Request, res: Response) {
   try {
     const { token, nickname } = loginScheme.validateSync(req.body);
     const email = await checkFirebase(token);
     if (!email) {
+      // 올바르지 않은 firebase token;
       return res
         .status(400)
         .json({ success: false, msg: `email isn't valid.` });
     }
-    const photoPath = email.split('@')[0];
-    const user: IUser = { email, nickname, photoPath };
-    const isExisted = await UserDao.findAndUpdate(user);
+    const userPhotoPath = email.split('@')[0];
+    const user: IUser = { email, nickname, photoPath: userPhotoPath };
+    let isExisted = await UserDao.findAndUpdate(user);
     if (!isExisted) {
       // 기존 사용자 없음
-      const result = await UserDao.insert(user);
-      logger.info(`POST /user | CREATE USER ${nickname}`);
-      return result
-        ? res.status(201).json({
-            user: {
-              email: user.email,
-              nickname: user.nickname,
-            },
-          })
-        : res.status(400).json({
-            success: false,
-            msg: `user can't create this server.`,
-          });
+      const isEnrolled = await UserDao.insert(user);
+      logger.info(`POST /user | CREATE USER ${nickname} - ${isEnrolled}`);
+    } else {
+      logger.info(`POST /user | LOGIN USER ${nickname}`);
     }
-    logger.info(`POST /user | LOGIN USER ${nickname}`);
-    return res.status(200).json({
-      user: {
-        email: isExisted.email,
-        nickname: isExisted.nickname,
-      },
-    });
+    // photos upload
+    const files = req.files as Express.Multer.File[];
+    const isUpload = uploadPhotos(userPhotoPath, files);
+
+    return isUpload
+      ? res.status(201).json({
+          user: {
+            email: user.email,
+            nickname: user.nickname,
+          },
+        })
+      : res.status(400).json({ success: false, msg: 'Failed photo upload' });
   } catch (e) {
     logger.error(e);
     return res.status(400).json({ success: false, msg: e.message });
   }
 }
 
-const router = express.Router();
-router.post('/', login);
+function uploadPhotos(userPhotoPath: string, files: Express.Multer.File[]) {
+  try {
+    const filenames = files.map((f) =>
+      f.fieldname === 'photos' ? f.filename : null
+    );
+    !fs.existsSync(`${photoPath}/${userPhotoPath}`) &&
+      fs.mkdirSync(`${photoPath}/${userPhotoPath}`);
+    for (const name in filenames) {
+      const oldname = `${photoPath}/${filenames[name]}`;
+      const newname = `${photoPath}/${userPhotoPath}/${userPhotoPath}-${name}.jpg`;
+      fs.rename(oldname, newname, function (err) {
+        if (err) throw err;
+      });
+    }
+    return true;
+  } catch (e) {
+    logger.error(e);
+    return false;
+  }
+}
 
+const router = express.Router();
+router.post('/', [upload.array('photos')], login);
 export { router as userRouter };
