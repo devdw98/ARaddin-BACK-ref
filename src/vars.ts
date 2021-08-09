2fc8c86cee0fbf306855b58e0057a0211ac2f27e
dw <devdw98@gmail.com>
2021-08-09 14:43:25 +0900

    upload files with user2

--- src/vars.ts
+++ src/vars.ts
@@ -5,9 +5,18 @@ dotenv.config({
   path: process.env.NODE_ENV === 'production' ? undefined : '.env',
 });
 
-export const { firebaseFile, photoPath } = yup
+export const {
+  firebaseFile,
+  rootPhotoPath,
+  userPhotoPath,
+  roomPhotoPath,
+  aiServer,
+} = yup
   .object({
     firebaseFile: yup.string().required(),
-    photoPath: yup.string(),
+    rootPhotoPath: yup.string(),
+    userPhotoPath: yup.string(),
+    roomPhotoPath: yup.string(),
+    aiServer: yup.string(),
   })
   .validateSync(process.env);
